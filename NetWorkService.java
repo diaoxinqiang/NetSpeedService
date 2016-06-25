7b6bf3f0656bf9dbfce02b104c22ec08e578876e
diaoxinqiang <qaz60437352@gmail.com>
2016-06-25 11:43:24 +0800

    logic optimize

--- NetWorkService.java
+++ NetWorkService.java
@@ -21,7 +21,7 @@ public class NetWorkService extends Service {
     private double rxtxSpeed = 1.0f;
     private DecimalFormat showFloatFormat = new DecimalFormat("0.00");
     private Intent receiverIntent;
-    public final static String NET_SPEED_RECEIVER_ACTION = "com.network_speed_action";
+    public final static String NET_SPEED_RECEIVER_ACTION = "com.ridgepm.network_speed_action";
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -29,13 +29,20 @@ public class NetWorkService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public void onCreate() {
+        super.onCreate();
         if (timer == null) {
             timer = new Timer();
-            timer.scheduleAtFixedRate(new RefreshTask(), 0L, (long) 2000);
+            timer.scheduleAtFixedRate(new RefreshTask(), 0L, (long) 1000);
         }
+
         receiverIntent = new Intent();
         receiverIntent.setAction(NET_SPEED_RECEIVER_ACTION);
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
         int result = super.onStartCommand(intent, flags, startId);
         return result;
     }
@@ -46,17 +53,23 @@ public class NetWorkService extends Service {
         // Service被终止的同时也停止定时器继续运行
         timer.cancel();
         timer = null;
+        Log.i("NetworkSpeedService", "ondestroy");
     }
 
     class RefreshTask extends TimerTask {
+        boolean isFirst = true;
 
         @Override
         public void run() {
             isNetBad = false;
             long tempSum = TrafficStats.getTotalRxBytes()
                     + TrafficStats.getTotalTxBytes();
+            if (isFirst) {
+                rxtxTotal = tempSum;
+                isFirst = false;
+            }
             long rxtxLast = tempSum - rxtxTotal;
-            double tempSpeed = rxtxLast * 1000 / 2000;
+            double tempSpeed = rxtxLast * 1000 / 1000;
             rxtxTotal = tempSum;
             if ((tempSpeed / 1024d) < 20 && (rxtxSpeed / 1024d) < 20) {
                 time += 1;
@@ -64,8 +77,8 @@ public class NetWorkService extends Service {
                 time = 0;
             }
             rxtxSpeed = tempSpeed;
-                Log.i("NetworkSpeedService", showFloatFormat.format(tempSpeed / 1024d) + "kb/s");
-            if (time >= 4) {//连续四次检测网速都小于10kb/s  断定网速很差.
+            Log.i("NetworkSpeedService", showFloatFormat.format(tempSpeed / 1024d) + "kb/s");
+            if (time >= 5) {//连续四次检测网速都小于10kb/s  断定网速很差.
                 isNetBad = true;
                 Log.i("NetworkSpeedService", "网速差 " + isNetBad);
                 time = 0; //重新检测
@@ -76,8 +89,10 @@ public class NetWorkService extends Service {
                     public void run() {
                         receiverIntent.putExtra("is_slow_net_speed", isNetBad);
                         sendBroadcast(receiverIntent);
+//                        NetWorkService.this.stopSelf();
                     }
                 });
+
             }
         }
 
